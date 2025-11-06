b1ba9e256221fab77ce5913ad2eda29f926b2a46
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-11-06 20:00:21 -0300

    feat(db): adicionar gerenciamento de banco de dados

---
+++ app/database_manager.py
@@ -0,0 +1,18 @@
+import psycopg2
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        db_url = current_app.config['DB_URL']
+        g.db = psycopg2.connect(db_url)
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
